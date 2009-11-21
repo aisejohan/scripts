58b2cc03b342e53ca7025a4ecdf1b9a76291d2c5
Aise Johan de Jong -- Hornet <aise.johan.de.jong@gmail.com>
2009-11-20 19:57:44 -0500

    Added characteristic polynomials

---
+++ fpols.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+prepare_list_grobner()
+{
+	cat tmp/grobner-$d1-$d2-$d3-$d4-$d \
+		| grep -v Coefficient \
+		| grep -v Allocate \
+		> tmp/list_grobner
+}
+
+run_gp()
+{
+	cat tmp/list_grobner | while read lijn
+	do
+		naam=$(echo "$lijn" | sed "s/ /-/g")
+		gp -q tmp/$d1-$d2-$d3-$d4-$d/$naam do_it.gp >> tmp/$d1-$d2-$d3-$d4-$d/$naam
+	done
+}
+
+cat tmp/list | while read line
+do
+	arr=($line)
+	d1=${arr[0]}
+	d2=${arr[1]}
+	d3=${arr[2]}
+	d4=${arr[3]}
+	d=${arr[4]}
+	prepare_list_grobner
+	run_gp
+done
